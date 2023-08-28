57421d0f76090d1c13b47f66b7db3c321bde0b6b
yeguu <qsc130@gmail.com>
2023-08-28 06:25:42 +0000

    11일차 문제 풀이 수정

--- Week3/Day11/Solution.js
+++ Week3/Day11/Solution.js
@@ -13,20 +13,14 @@ rl.on("line", (line) => {
   let ache = lines[0][0]; // 통증 수치
   const [a, b] = lines[1]; // 아이템 A, B
 
-  // 아이템 B만으로 나누어 떨어지는 경우
-  if (ache % b === 0) {
-    console.log(parseInt(ache / b));
-    process.exit();
-  }
-
   // 아이템 B의 사용 개수
-  let bCnt = parseInt(ache / b);
+  let bCnt = Math.floor(ache / b);
 
   // 아이템 B의 개수를 1씩 줄여가며 아이템 A로 0 만들 수 있는지 확인
-  while (bCnt > -1) {
+  while (bCnt >= 0) {
     let acheB = ache - bCnt * b;
     if (acheB % a === 0) {
-      console.log(bCnt + parseInt(acheB / a));
+      console.log(bCnt + Math.floor(acheB / a));
       process.exit();
     } else {
       bCnt--;
